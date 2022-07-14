47fd762fafcb0043a0a4c1337396593d0594863c
Nguyen Dang Minh <83014500+dangminh71220002@users.noreply.github.com>
2022-07-14 23:43:50 +0700

    Get API ( File json)

--- Lab2/App.js
+++ Lab2/App.js
@@ -6,15 +6,29 @@ import React, { useState, useEffect } from 'react';
 import { SafeAreaView, Text, StyleSheet, View, FlatList,Image,TouchableOpacity} from 'react-native';
 import { SearchBar } from 'react-native-elements';
 
+const Search = ({navigation}) => {
+  const [search, setSearch] = useState('');
+  const [filteredDataSource, setFilteredDataSource] = useState([]);
+  const [masterDataSource, setMasterDataSource] = useState([]);
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Minh</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [music, setMusic] = useState([]);
+    const getMusicFromApi = async () => {
+        const resp = await fetch("https://us-central1-musdio-6ec90.cloudfunctions.net/app/api/music/get")
+        const data = await resp.json().then((responseJson) => {
+          setFilteredDataSource(responseJson.data);
+          setMasterDataSource(responseJson.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+        return data.data
+    };
+    useEffect(() => {
+        if (music.length===0){  setMusic(getMusicFromApi()) }
+    }, [])
+
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -24,3 +38,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+export default Search
