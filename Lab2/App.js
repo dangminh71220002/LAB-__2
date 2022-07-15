efc0391a92b79752d92adaf5437719e2c1a02f30
Nguyen Dang Minh <83014500+dangminh71220002@users.noreply.github.com>
2022-07-15 13:46:58 +0700

    Use data by search( Search Song and singer )

--- Lab2/App.js
+++ Lab2/App.js
@@ -28,7 +28,62 @@ const Search = ({navigation}) => {
     }, [])
 
 
-
+    const searchFilterFunction = (text) => {
+      // Check if searched text is not blank
+      if (text) {
+        // Inserted text is not blank
+        // Filter the masterDataSource
+        // Update FilteredDataSource
+        const newData = masterDataSource.filter(function (item) {
+          const input=item.name+' - '+item.singer;
+          const itemData = item.name+'-'+item.singer
+            ? input.toUpperCase()
+            : ''.toUpperCase();
+     
+            
+          const textData = text.toUpperCase();
+          return itemData.indexOf(textData) > -1;
+        });
+        setFilteredDataSource(newData);
+        setSearch(text);
+      } else {
+        // Inserted text is blank
+        // Update FilteredDataSource with masterDataSource
+        setFilteredDataSource(masterDataSource);
+        setSearch(text);
+      }
+    };
+  
+    const ItemView = ({ item }) => {
+      const input=item.name+'-'+item.singer;
+      return (
+        // Flat List Item
+      <TouchableOpacity onPress={() => getItem(item)}>
+        <View style={styles.itemView} >
+          <Image style={styles.itemStyle} source={{uri:item.img}}/>
+          <Text style={{fontWeight:'bold'}}>
+            {input.toUpperCase() }
+          </Text>
+        </View>
+      </TouchableOpacity>
+        
+        
+      );
+    };
+  
+    const ItemSeparatorView = () => {
+      return (
+        // Flat List Item Separator
+        <View
+          style={{
+            height: 0.5,
+            width: '100%',
+            backgroundColor: '#C8C8C8',
+          }}
+        />
+      );
+    };
+  
     const getItem = (item) => {
       // Function for click on an item
       alert('Id : ' + item.id + ' name : ' + item.name+' singer: '+item.singer);
